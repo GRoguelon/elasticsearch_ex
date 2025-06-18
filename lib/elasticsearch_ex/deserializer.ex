fdb1e27c6c3e8c17d9034249419975d3a173bc88
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2025-06-17 20:08:47 -0600

    Updated the deserializer to make it easier

--- lib/elasticsearch_ex/deserializer.ex
+++ lib/elasticsearch_ex/deserializer.ex
@@ -4,7 +4,7 @@ defmodule ElasticsearchEx.Deserializer do
 
   This module deserializes Elasticsearch responses, transforming string-keyed maps into Elixir types
   such as `Range`, `Date.Range`, `Date`, or `DateTime` based on the provided mappings. It supports
-  nested maps, lists, and streams, and allows custom key transformation via a `key_fun`.
+  nested maps, lists, and streams, and allows custom key transformation via a `key_mapper`.
 
   ## Supported Types
   - `"binary"`: Decodes base64-encoded strings.
@@ -24,6 +24,9 @@ defmodule ElasticsearchEx.Deserializer do
       Stream.run(deserialize(stream, mapping))
   """
 
+  alias ElasticsearchEx.MapExt
+  alias ElasticsearchEx.MappingsCacher
+
   require Logger
 
   ## Module attributes
@@ -34,11 +37,6 @@ defmodule ElasticsearchEx.Deserializer do
   Must contain a `"properties"` key when provided as a map.
   """
   @type mappings :: %{required(binary()) => any()}
-
-  @typedoc """
-  A mapper, either a function that takes an index name and returns mappings, or a mappings map.
-  """
-  @type mapper :: (binary() -> mappings()) | mappings()
 
   @typedoc """
   Input data: a stream, list of documents, a single document, a document source, or a field value.
@@ -53,8 +51,7 @@ defmodule ElasticsearchEx.Deserializer do
   Deserializes a list of documents, a single document, a document source, or a stream.
 
   - `value`: The input data (stream, list, document, or source map).
-  - `mapper`: A function that takes an index name and returns mappings, or a mappings map with a `"properties"` key.
-  - `key_fun`: A function to transform keys in deserialized maps (defaults to `Function.identity/1`).
+  - `key_mapper`: A function to transform keys in deserialized maps (defaults to `Function.identity/1`).
 
   Returns a stream for stream input, a list for list input, or a map for document/source input.
 
@@ -75,41 +72,46 @@ defmodule ElasticsearchEx.Deserializer do
       deserialize(document, mapping)
       # => %{"_index" => "test", "_source" => %{"date" => "invalid"}}
   """
-  @spec deserialize(value(), mapper(), (binary() -> any())) :: value()
-  def deserialize(value, mapper, key_fun \\ &Function.identity/1)
+  @spec deserialize(value(), (binary() -> any())) :: value()
+  def deserialize(value, key_mapper \\ &Function.identity/1)
 
-  def deserialize(stream, mapper_fun, key_fun) when is_struct(stream, Stream) do
-    Stream.map(stream, &deserialize(&1, mapper_fun, key_fun))
+  def deserialize(stream, key_mapper) when is_struct(stream, Stream) do
+    Stream.map(stream, &deserialize(&1, key_mapper))
   end
 
-  def deserialize(values, mapper_fun, key_fun) when is_list(values) do
-    Enum.map(values, &deserialize(&1, mapper_fun, key_fun))
+  def deserialize(values, key_mapper) when is_list(values) do
+    Enum.map(values, &deserialize(&1, key_mapper))
   end
 
-  def deserialize(%{"hits" => %{"hits" => hits}} = result, mapper_fun, key_fun) do
-    deserialized_hits = deserialize(hits, mapper_fun, key_fun)
+  @key_accesses ~w[hits hits]
 
-    put_in(result["hits"]["hits"], deserialized_hits)
+  def deserialize(%{"hits" => %{"hits" => _hits}} = result, key_mapper) do
+    {hits, result} = pop_in(result, @key_accesses)
+    atomized_result = MapExt.map_keys(result, key_mapper)
+    deserialized_hits = deserialize(hits, key_mapper)
+    key_accesses = Enum.map(@key_accesses, key_mapper)
+
+    put_in(atomized_result, key_accesses, deserialized_hits)
   end
 
-  def deserialize(
-        %{"_index" => index, "_source" => source} = document,
-        mapping_or_mapper_fun,
-        key_fun
-      ) do
-    mapping = ensure_mapping!(mapping_or_mapper_fun, index)
+  def deserialize(%{"_index" => index, "_source" => _source} = document, key_mapper) do
+    mapping = MappingsCacher.get(index)
+    {source, document} = Map.pop!(document, "_source")
+    atomized_document = MapExt.map_keys(document, key_mapper)
+    deserialized_source = deserialize_field(source, mapping, key_mapper)
+    mapped_key = key_mapper.("_source")
 
-    deserialized_source = deserialize_field(source, mapping, key_fun)
-
-    put_in(document["_source"], deserialized_source)
+    Map.put(atomized_document, mapped_key, deserialized_source)
   end
+
+  def deserialize(document, key_mapper), do: MapExt.map_keys(document, key_mapper)
 
   @doc """
   Deserializes a field value based on its Elasticsearch mapping.
 
   - `value`: The field value (map, list, or scalar).
   - `mapping`: The field’s mapping, typically containing `"type"` and optional `"format"`.
-  - `key_fun`: A function to transform keys in deserialized maps (defaults to `Function.identity/1`).
+  - `key_mapper`: A function to transform keys in deserialized maps (defaults to `Function.identity/1`).
 
   Supports specific types like `"binary"`, `"integer_range"`, `"long_range"`, `"date_range"`,
   and `"date"`. Non-matching values or invalid formats are returned unchanged.
@@ -127,18 +129,18 @@ defmodule ElasticsearchEx.Deserializer do
       # => Date.Range.t()
   """
   @spec deserialize_field(any(), mappings(), (binary() -> any())) :: any()
-  def deserialize_field(value, mapping, key_fun \\ &Function.identity/1)
+  def deserialize_field(value, mapping, key_mapper \\ &Function.identity/1)
 
-  def deserialize_field(value, mapping, key_fun) when is_list(value) do
-    Enum.map(value, &deserialize_field(&1, mapping, key_fun))
+  def deserialize_field(value, mapping, key_mapper) when is_list(value) do
+    Enum.map(value, &deserialize_field(&1, mapping, key_mapper))
   end
 
-  def deserialize_field(value, %{"properties" => mapping}, key_fun) when is_map(value) do
+  def deserialize_field(value, %{"properties" => mapping}, key_mapper) when is_map(value) do
     Map.new(value, fn {key, value} ->
       key_mapping = Map.fetch!(mapping, key)
-      deserialized_value = deserialize_field(value, key_mapping, key_fun)
+      deserialized_value = deserialize_field(value, key_mapping, key_mapper)
 
-      {key_fun.(key), deserialized_value}
+      {key_mapper.(key), deserialized_value}
     end)
   end
 
@@ -198,20 +200,5 @@ defmodule ElasticsearchEx.Deserializer do
 
   def deserialize_field(value, _mapping, _) do
     value
-  end
-
-  ## Private functions
-
-  defp ensure_mapping!(mapping_or_mapper_fun, index) do
-    cond do
-      is_function(mapping_or_mapper_fun, 1) ->
-        mapping_or_mapper_fun.(index)
-
-      is_map(mapping_or_mapper_fun) and is_map_key(mapping_or_mapper_fun, "properties") ->
-        mapping_or_mapper_fun
-
-      true ->
-        raise ArgumentError, "mapper argument must be a map or a function of arity 1"
-    end
   end
 end
